8a4c2cfd374d72ef2f6702129c67405dd34889f5
Disha Talreja <disha@Dishas-MacBook-Air.local>
2023-07-13 16:16:00 +0530

    initial commit

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,21 +1,44 @@
 <template>
-  <Page>
-    <ActionBar>
-      <Label text="Home"/>
-    </ActionBar>
 
-    <GridLayout>
-      <Label class="info">
-        <FormattedString>
-          <Span class="fas" text.decode="&#xf135; "/>
-          <Span :text="message"/>
-        </FormattedString>
+  
+  <Page>
+    <ActionBar title="Action Items">
+        <ActionItem tap="onShare"
+            ios.systemIcon="9" ios.position="left"
+            android.systemIcon="ic_menu_share" android.position="actionBar"></ActionItem>
+        <ActionItem tap="onDelete" text="delete" 
+            ios.systemIcon="16" ios.position="right"
+            android.position="popup"></ActionItem>
+    </ActionBar>
+    
+    
+    
+    
+    
+    
+
+    <AbsoluteLayout>
+      <Label text="10,10" left="10" top="10" width="100" height="100" backgroundColor="#43b883">
+        
       </Label>
-    </GridLayout>
+      <HtmlView top="400" html="
+    <div>
+      <h1>HtmlView</h1>
+    </div>
+    <div class='box'>
+      This is a box
+    </div>
+    "></HtmlView>
+      <Button top="200"  left="300" class="my-button" text="Tap me!"></Button>
+      <Label text="120,10" left="120" top="10" width="100" height="100" backgroundColor="#43b883"/>
+      <Label text="10,120" left="10" top="120" width="100" height="100" backgroundColor="#43b883"/>
+      <Label text="120,120" left="120" top="120" width="100" height="100" backgroundColor="#43b883"/>
+    </AbsoluteLayout>
   </Page>
 </template>
 
 <script lang="ts">
+// import { HtmlView } from "@nativescript/core";
   import Vue from "nativescript-vue";
 
   export default Vue.extend({
@@ -23,7 +46,7 @@
       message() {
         return "Blank {N}-Vue app";
       }
-    }
+    },
   });
 </script>
 
@@ -36,8 +59,19 @@
   }
 
   .info {
-    font-size: 20;
+    font-size: 50;
     horizontal-align: center;
     vertical-align: center;
   }
+    .my-button {
+    background-color: lightseagreen;
+    border-color: black;
+    border-radius: 20;
+    border-width: 2;
+    color: whitesmoke;
+    font-size: 18;
+    font-weight: bold;
+    height: 100px;
+    width: 200px;
+}
 </style>
